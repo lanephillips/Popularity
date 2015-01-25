a4cdadcbea3b1844081cea865b1257205eaf0def
Lane Phillips <lane@milkllc.com>
2015-01-24 22:39:33 -0600

    center map on user

--- Popularity/ViewController.m
+++ Popularity/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+@import CoreLocation;
+@import MapKit;
 
 @interface ViewController ()
+<CLLocationManagerDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *map;
+@property (nonatomic) CLLocationManager* locationManager;
 
 @end
 
@@ -25,7 +31,64 @@
 }
 
 - (IBAction)doCenter:(id)sender {
-    // TODO: get user's location
+    if (nil == self.locationManager) {
+        self.locationManager = [[CLLocationManager alloc] init];
+        self.locationManager.delegate = self;
+    }
+
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
+        [self.locationManager requestWhenInUseAuthorization];
+    } else {
+        [self getLocation];
+    }
+}
+
+- (void)getLocation {
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    // TODO: start a spinner
+    [self.locationManager startUpdatingLocation];
+}
+
+#pragma mark - location delegate
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    switch (status) {
+        case kCLAuthorizationStatusAuthorizedAlways:
+        case kCLAuthorizationStatusAuthorizedWhenInUse:
+            [self getLocation];
+            break;
+            
+        default:
+            break;
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation* location = [locations lastObject];
+    NSLog(@"got location: %f, %f", location.coordinate.latitude, location.coordinate.longitude);
+    
+    // TODO: save last map location for startup next time
+    
+    // go ahead and update the map
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 500, 500);
+    MKCoordinateRegion adjustedRegion = [self.map regionThatFits:viewRegion];
+    self.map.showsUserLocation = YES;
+    [self.map setRegion:adjustedRegion animated:YES];
+    
+    // but wait until we have a fresh result to run the query
+    NSDate* eventDate = location.timestamp;
+    NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
+    if (abs(howRecent) < 15.0) {
+        [self.locationManager stopUpdatingLocation];
+        
+        // TODO: query businesses
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"%@", error);
+    [self.locationManager stopUpdatingLocation];
 }
 
 @end
